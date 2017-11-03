d1dd7cc661d4691890c2cec9dd196528c7c347f6
Matt Finnell <mattfinnell104@gmail.com>
2017-11-03 07:59:22 +0000

    Tweaked setup.sh on blank VM

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
+# [ VIM Configuration ]
+git clone https://github.com/mattfinnell/vim-configuration.git ~/.vim
+
+# [ TMUX Configuration ]
+# apt-get install -y tmux
+
+ln --symbolic $PWD/tmux.conf ~/.tmux.conf
+
 # [ ZSH Configuration ]
 apt-get install -y zsh
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-ln --symbolic zshrc ~/.zshrc
+ln --symbolic $PWD/zshrc ~/.zshrc
 
 chsh -s /bin/zsh
+echo "\nUse ZSH instead of Bash\n/usr/bin/zsh"
 
-# [ TMUX Configuration ]
-apt-get install -y tmux
-
-ln --symbolic tmux.conf ~/tmux.conf
+/usr/bin/zsh
