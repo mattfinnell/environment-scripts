02349d90304e16533ad3e8297b3b97c0086dc140
Matt Finnell <mattfinnell104@gmail.com>
2017-11-03 08:46:32 +0000

    Tightened up setup.sh

--- setup.sh
+++ setup.sh
@@ -2,16 +2,15 @@
 git clone https://github.com/mattfinnell/vim-configuration.git ~/.vim
 
 # [ TMUX Configuration ]
-apt-get install -y tmux
+sudo apt-get install -y tmux
 
 ln --symbolic $PWD/tmux.conf ~/.tmux.conf
 
 # [ ZSH Configuration ]
-apt-get install -y zsh
+sudo apt-get install -y zsh
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 ln --symbolic $PWD/zshrc ~/.zshrc
 
-chsh -s /bin/zsh
-echo "\nUse ZSH instead of Bash\n/usr/bin/zsh" >> ~/.bashrc
+chsh -s /usr/bin/zsh $USER
